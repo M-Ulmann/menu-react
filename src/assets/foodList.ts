a4bcacd453834bbab59676f1455f2fe32e058581
M-Ulmann <marek.ulmannek@gmail.com>
2026-07-01 12:42:46 +0200

    Pridany soubor se seznamem jidel

---
+++ src/assets/foodList.ts
@@ -0,0 +1,71 @@
+export const foodList = [{
+  name: 'Buttermilk Pancakes',
+  price: 1599,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-1.jpeg",
+  description: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed`,
+
+  type: 'breakfast'
+}, {
+  name: 'Diner Double',
+  price: 1399,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-2.jpeg",
+  description: 'vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats ',
+
+  type: 'lunch'
+}, {
+  name: 'Godzilla Milkshake',
+  price: 699,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-3.jpeg",
+  description: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+
+  type: 'shakes'
+}, {
+  name: 'Country Delight',
+  price: 2099,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-4.jpeg",
+  description: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,`,
+
+  type: 'breakfast'
+}, {
+  name: 'Egg Attack',
+  price: 2299,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-5.jpeg",
+  description: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up`,
+
+  type: 'lunch'
+}, {
+  name: 'Oreo Dream',
+  price: 1899,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-6.jpeg",
+  description: 'Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday',
+
+  type: 'shakes'
+}, {
+  name: 'Bacon Overflow',
+  price: 899,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-7.jpeg",
+  description: "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
+
+  type: 'breakfast'
+}, {
+  name: 'American Classic',
+  price: 1299,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-8.jpeg",
+  description: "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut",
+
+  type: 'lunch'
+}, {
+  name: 'Quarantine Buddy',
+  price: 1699,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-9.jpeg",
+  description: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
+
+  type: 'shakes'
+}, {
+  name: 'Steak Dinner',
+  price: 3999,
+  picture: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-10.jpeg",
+  description: "lorem ipsum com passo i dont know what to type here some moccha chan ga oshiete kureta",
+
+  type: 'dinner'
+}];
